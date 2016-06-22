b2a33c32aa75081b901e43d1891ad9d259944b71
Jesse A. Tov <jesse.tov@gmail.com>
2016-06-22 13:45:10 -0500

    Updated Travis; trying again.

---
+++ info.rkt
@@ -0,0 +1,10 @@
+#lang info
+(define collection "dssl")
+(define deps '("base"
+;                "rackunit-lib"
+               ))
+; (define build-deps '("scribble-lib" "racket-doc"))
+; (define scribblings '(("scribblings/mehhh.scrbl" ())))
+(define pkg-desc "Data Structures Student Language")
+(define version "0.1")
+(define pkg-authors '(tov))
