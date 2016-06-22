67ec7ba20f1de06c0b90d9f709ff628e1b017a9d
Jesse A. Tov <jesse.tov@gmail.com>
2016-06-22 11:10:02 -0500

    Added all special forms.

--- dssl.rkt
+++ dssl.rkt
@@ -5,12 +5,18 @@
            [test                run-all-tests])
          ;; from Advanced Student Language:
          (except-out (all-from-out lang/htdp-advanced)
+                     case
                      cond
                      lambda
                      λ
                      let
                      let*
+                     letrec
                      local
+                     match
+                     recur
+                     shared
+                     time
                      unless
                      when)
          ;; Our own definitions:
@@ -19,12 +25,18 @@
            [dssl-define         define]
            [dssl-define-struct  define-struct]
            ;; Based on the ASL version:
+           [dssl-case           case]
            [dssl-cond           cond]
            [dssl-lambda         lambda]
            [dssl-lambda         λ]
            [dssl-let            let]
            [dssl-let*           let*]
+           [dssl-letrec         letrec]
+           [dssl-match          match]
+           [dssl-recur          recur]
+           [dssl-shared         shared]
            [dssl-local          local]
+           [dssl-time           time]
            [dssl-unless         unless]
            [dssl-when           when]))
 
@@ -42,6 +54,17 @@
 (require test-engine/racket-tests)
 
 (require (for-syntax "classes.rkt"))
+
+(define-syntax-parser dssl-case
+  #:literals (else)
+  [(_ [(choice:expr ...) expr:expr ...+] ...+)
+   #'(case [(choice ...) (begin expr ...)] ...)]
+  [(_ [(choice:expr ...) expr:expr ...+]
+      ...
+      [else last:expr ...])
+   #'(cond [(choice ...) (begin expr ...)]
+           ...
+           [else (begin last ...)])])
 
 (define-syntax-parser dssl-cond
   #:literals (else)
@@ -76,8 +99,31 @@
 (define-simple-macro (dssl-let* bs:distinct-bindings expr:expr ...+)
   (let* bs (begin expr ...)))
 
+(define-simple-macro (dssl-letrec bs:distinct-bindings expr:expr ...+)
+  (letrec bs (begin expr ...)))
+
 (define-simple-macro (dssl-local (decl:expr ...) expr:expr ...+)
-  #'(local (decl ...) (begin expr ...)))
+  (local (decl ...) (begin expr ...)))
+
+(define-syntax-parser dssl-match
+  #:literals (else)
+  [(_ [pattern:expr expr:expr ...+] ...+)
+   #'(match [pattern (begin expr ...)] ...)]
+  [(_ [pattern:expr expr:expr ...+]
+      ...
+      [else last:expr ...])
+   #'(match [pattern (begin expr ...)]
+            ...
+            [else (begin last ...)])])
+
+(define-simple-macro (dssl-recur name:id bs:distinct-bindings expr:expr ...+)
+  (recur name bs (begin expr ...)))
+
+(define-simple-macro (dssl-shared bs:distinct-bindings expr:expr ...+)
+  (shared bs (begin expr ...)))
+
+(define-simple-macro (dssl-time expr:expr ...+)
+  (time (begin expr ...)))
 
 (define-simple-macro (dssl-unless test:expr expr:expr ...+)
   (unless test (begin expr ...)))
