4891c00f482faf77fd09e8639cce4feb7c2e7ea2
Jesse A. Tov <jesse.tov@gmail.com>
2016-10-27 08:36:05 -0500

    added build-dep: compatibility-doc

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "dssl")
 (define deps '("base" "htdp-lib"))
-(define build-deps '("scribble-lib" "racket-doc" "htdp-doc" "at-exp-lib" "sandbox-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "htdp-doc" "at-exp-lib" "sandbox-lib" "compatibility-doc"))
 (define scribblings '(("scribblings/dssl.scrbl" ())))
 (define pkg-desc "Data Structures Student Language")
 (define version "0.3")
